571e6a2b8d3e094ed3e1ce7ffe6bc9fa33649ea5
wangz <jitong2326@163.com>
2022-08-18 17:46:31 +0800

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import robots from "./mockData/robots.json";
+import React, { useState } from "react";
 import Robot from "./components/Robot";
 import styles from "./App.module.css";
 import logo from "./assets/images/logo.svg";
@@ -10,36 +9,27 @@ interface Props {}
 interface State {
   robotGallery: any[];
 }
-class App extends React.Component<Props, State> {
-  constructor(props: Props | Readonly<Props>) {
-    super(props)
-    this.state = {
-      robotGallery: []
-    }
-  }
+const App : React.FC = (props) => {
+  const [count, setCount] = useState<number>(0)
 
-  componentDidMount(): void {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => this.setState({ robotGallery: data }))
-  }
-
-  render() {
     return (
       <div className={styles.app}>
         <div className={styles.appHeader}>
           <img src={logo} alt="logo" className={styles.appLogo} />
           <h1>robot</h1>
         </div>
+        <button onClick={() => {
+          setCount(count+1)
+        }}>click</button>
+        <span>{count}</span>
         <ShoppingCart />
-        <div className={styles.robotList}>
+        {/* <div className={styles.robotList}>
           {this.state.robotGallery.map((r) => (
             <Robot id={r.id} name={r.name} email={r.email} />
           ))}
-        </div>
+        </div> */}
       </div>
-    );
-  }
+    )
 }
 
 export default App;
