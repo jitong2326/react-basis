7fd6269932183289ffe5aa50cc54291c145ea5b8
wangz <jitong2326@163.com>
2022-08-19 14:11:05 +0800

    useContext

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./Robot.module.css";
-
+import { appContext } from "../index";
 interface RobotProps {
-  id: number
-  name: string
-  email: string
+  id: number;
+  name: string;
+  email: string;
 }
 
-const Robot : React.FC<RobotProps> = ({id, name, email}) => {
+const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+  const value = useContext(appContext);
   return (
     <div className={styles.cardContainer}>
       <img src={`https://robohash.org/${id}`} alt="robot" />
       <h2>{name}</h2>
       <p>{email}</p>
+      <p>作者：{value.userName}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Robot
+export default Robot;
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const defaultContextValue = {
+  userName: 'jitong'
+}
+
+export const appContext = React.createContext(defaultContextValue)
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <appContext.Provider value={defaultContextValue}>
+      <App />
+    </appContext.Provider>
   </React.StrictMode>
 );
 
